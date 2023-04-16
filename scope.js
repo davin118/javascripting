c693dd44d1684a745eca40beebc8403fab72d30f
Davin Ortez <davin.ortez118@gmail.com>
2023-04-16 15:00:38 -0600

    Informacion

--- scope.js
+++ scope.js
@@ -4,9 +4,8 @@ const a = 1; const b = 2; const c = 3;
        const b = 5; const c = 6;  
        
        (function secondFunction () {  
-         const b = 8;  
-         //console.log('a: ' + a + ' b: '+b+' c: '+c)
-         console.log(`a: ${a}, b: ${b}, c: ${c}`);
+         const b = 8; 
+        console.log(`a: ${a}, b: ${b}, c: ${c}`);
          (function thirdFunction () {  
            const a = 7; const c = 9;  
        
